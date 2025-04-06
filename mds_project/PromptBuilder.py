e58513e95300ed42eeda13ea96e94c2511d1156b
ayalaajil <aya.laajil@student-cs.fr>
2025-04-06 08:43:55 +0200

    Omar commit

--- mds_project/PromptBuilder.py
+++ mds_project/PromptBuilder.py
@@ -1,8 +1,7 @@
 from typing import List
 
 class PromptBuilder:
-    
-    def _init_(self) -> None:
+    def init(self) -> None:
         """
         Initialize the prompt generator.
         """
@@ -12,8 +11,7 @@ class PromptBuilder:
     def build_prompt(
             self,
             symptoms : list , 
-            description : list ,
-            meta : list , 
+            meta_str : str , 
             detail_level: float, 
             enumeration: bool = False, 
             explicit_symptom: bool = False, 
@@ -26,7 +24,6 @@ class PromptBuilder:
         """
         Construct a prompt based on the given parameters.
         """
-
         if not isinstance(symptoms, list) or not all(isinstance(s, str) for s in symptoms):
             raise ValueError("symptoms must be a list of strings.")
         
@@ -39,32 +36,17 @@ class PromptBuilder:
             {
                 "role": "user",
                 "content": (
-                    f"Write a phrase from a sick patient describing their symptoms. The symptoms the patient describes are: {', '.join(symptoms)}. "
+                    f"A sick patient filled out a medical form . From the information he filled out , generate a phrase putting your shoes into this patient describing its symptoms. The symptoms the patient describes are: {', '.join(symptoms)}. "
                     f"The patient speaks in {language_style} Register and uses a {tone} tone. "
                 )
             }
         ]
         
-
-        if isinstance(meta, list):
-
-            # Filter out unwanted entries
-            filtered_meta = [
-                (desc, val) for desc, val in zip(description, meta)
-                if val not in ['None', 'Prefer not to answer']]
-            
-            if filtered_meta:
-
-                descs, metas = zip(*filtered_meta)
-                messages[1]['content'] += (
-                    f"The {list(descs)} of the symptoms correspond to {list(metas)}, respectively. "
-                )
-
-        elif meta not in ['None', 'Prefer not to answer']:
-
-            messages[1]['content'] += (
-                f"The {description} of the symptom correspond to {meta}. "
+        
+        messages[1]['content'] += (
+                f"The informations he filled for these symptoms are the following :  {meta_str} , the phrase should be generated based on these informations"
             )
+        
 
         # Specify the level of detail
         detail_instructions = {
@@ -83,7 +65,7 @@ class PromptBuilder:
             )
 
         if explicit_symptom:
-            messages[1]['content'] += f"The patient should explicitly mention the symptoms they have (e.g., {', '.join(symptoms)}). "
+            messages[1]['content'] += f"The patient should explicitly mention the symptoms they have (here {', '.join(symptoms)} should be all mentioned) "
 
         if spelling_errors:
             messages[1]['content'] += " Include spelling mistakes in the generated phrase."
@@ -91,3 +73,98 @@ class PromptBuilder:
         messages[1]['content'] += " Respond with only the sentence in the patient's voice, enclosed in double quotes, without any additional text, comments, or notes."
 
         return messages
+
+
+# from typing import List
+
+# class PromptBuilder:
+    
+#     def _init_(self) -> None:
+#         """
+#         Initialize the prompt generator.
+#         """
+#         pass
+
+
+#     def build_prompt(
+#             self,
+#             symptoms : list , 
+#             description : list ,
+#             meta : list , 
+#             detail_level: float, 
+#             enumeration: bool = False, 
+#             explicit_symptom: bool = False, 
+#             language_style: str = "everyday language", 
+#             spelling_errors: bool = False, 
+#             tone: str = "neutral"
+
+#             ) -> list[dict]:
+        
+#         """
+#         Construct a prompt based on the given parameters.
+#         """
+
+#         if not isinstance(symptoms, list) or not all(isinstance(s, str) for s in symptoms):
+#             raise ValueError("symptoms must be a list of strings.")
+        
+
+#         messages = [
+#             {
+#                 "role": "system",
+#                 "content": f"You are a chatbot that writes in the voice of a patient using a {language_style} register with a {tone} tone."
+#             },
+#             {
+#                 "role": "user",
+#                 "content": (
+#                     f"Write a phrase from a sick patient describing their symptoms. The symptoms the patient describes are: {', '.join(symptoms)}. "
+#                     f"The patient speaks in {language_style} Register and uses a {tone} tone. "
+#                 )
+#             }
+#         ]
+        
+
+#         if isinstance(meta, list):
+
+#             # Filter out unwanted entries
+#             filtered_meta = [
+#                 (desc, val) for desc, val in zip(description, meta)
+#                 if val not in ['None', 'Prefer not to answer']]
+            
+#             if filtered_meta:
+
+#                 descs, metas = zip(*filtered_meta)
+#                 messages[1]['content'] += (
+#                     f"The {list(descs)} of the symptoms correspond to {list(metas)}, respectively. "
+#                 )
+
+#         elif meta not in ['None', 'Prefer not to answer']:
+
+#             messages[1]['content'] += (
+#                 f"The {description} of the symptom correspond to {meta}. "
+#             )
+
+#         # Specify the level of detail
+#         detail_instructions = {
+#             1: "The description should be very brief with minimal details.",
+#             2: "The description should be brief with some basic details.",
+#             3: "The description should provide a moderate level of detail.",
+#             4: "The description should be detailed and thorough.",
+#             5: "The description should be very detailed and comprehensive."
+#         }
+#         messages[1]['content'] += detail_instructions.get(detail_level, "Provide a moderate level of detail.") + " "
+
+#         if enumeration:
+#             messages[1]['content'] += (
+#                 "The patient should just enumerate the descriptions. For example, for symptom Anemia, a sentence could be: "
+#                 "'Fatigue and Weakness, Pale Skin, Shortness of Breath, Dizziness, Irregular Heartbeat, Chest Pain, Cold Hands and Feet, Headaches'. "
+#             )
+
+#         if explicit_symptom:
+#             messages[1]['content'] += f"The patient should explicitly mention the symptoms they have (e.g., {', '.join(symptoms)}). "
+
+#         if spelling_errors:
+#             messages[1]['content'] += " Include spelling mistakes in the generated phrase."
+
+#         messages[1]['content'] += " Respond with only the sentence in the patient's voice, enclosed in double quotes, without any additional text, comments, or notes."
+
+#         return messages
---
+++ mds_project/generate_dataset_many_symptoms.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import random
+from autocorrect import Speller
+import itertools
+import re
+from LLM import LLM
+from PromptBuilder import PromptBuilder
+from metadata import language_registers, discussion_tones
+
+
+def get_combinations(symptom, symptom_data):
+    # Extract the attribute lists for the symptom
+    attributes = symptom_data[symptom]
+    # Get the attribute names (keys) and corresponding lists (values)
+    attribute_names = list(attributes.keys())
+    attribute_values = list(attributes.values())
+    
+    # Generate all combinations using itertools.product
+    combinations = list(itertools.product(*attribute_values))
+    
+    # Optionally, pair each combination with its corresponding attribute names:
+    combinations_named = [
+        dict(zip(attribute_names, combination)) for combination in combinations
+    ]
+    
+    return combinations_named
+
+
+model = LLM()
+prompt_builder = PromptBuilder()
+
+a = pd.read_excel('/home/laajila/mima_newcode/mds_project/PRO-CTCAE_Questionnaire_Terminology.xls', sheet_name = 'PRO')
+index = (a [a['PRO-CTCAE PT'] == 'Pain and swelling at injection site']).index.values[0] +1 
+symptoms = a.iloc[:index]['PRO-CTCAE PT'].values
+
+Dict = {}
+
+for symptom in symptoms : 
+
+    try:
+        Dict[symptom] = {}
+        descriptions_code = a[a['PRO-CTCAE PT'] == symptom]['Has PRO-CTCAE Attribute Code'].values[0].split(" || ")
+        descriptions = a [a['PRO-CTCAE PT'] == symptom]['Has PRO-CTCAE Attribute PT'].values[0].split(" || ")
+        for description in descriptions :
+            Dict[symptom][description]  = a[a['PRO-CTCAE PT'] == description  ]['PRO-CTCAE Value PT'].values[0].split(" || ")
+    
+    except Exception as e :
+        print(e)
+
+
+for el in Dict.keys():
+    for el2 in Dict[el].keys():
+        if "Not sexually active" in Dict[el][el2]:
+            Dict[el][el2].remove("Not sexually active")
+
+del Dict['Other Symptoms']
+
+print("The dictionary has been cleaned and is ready to be used")
+
+data = []
+
+counter = 0  # Initialize the counter
+
+total = sum(len(get_combinations(symptom, Dict)) for symptom in Dict.keys())  # Total combinations
+
+
+for symptom in Dict.keys():
+
+    combinaisons = get_combinations(symptom, Dict)
+
+    for combi in combinaisons :
+
+        descriptions =  list(combi.keys())
+
+        meta_descriptions = list(combi.items())
+
+        meta_str = " , ".join([f"{k} = {v}" for k, v in meta_descriptions])
+        
+        detail_level = np.random.choice([1,2,3,4,5])
+
+        enumeration = np.random.choice([True, False], p=[0.2, 0.8])
+
+        explicit_symptom = np.random.choice([True, False], p=[0.2, 0.8])
+
+        language_style = random.choice(language_registers)['name']
+
+        tone = random.choice(discussion_tones)['name']
+
+        spelling_errors = np.random.choice([True, False], p=[0.7, 0.3])
+
+        prompt = prompt_builder.build_prompt([symptom], meta_str, detail_level, enumeration, explicit_symptom, language_style, spelling_errors,
+         tone) 
+   
+        phrase_generated = model.generate_text(messages = prompt)
+
+        data.append([phrase_generated , symptom, descriptions, meta_str, language_style, tone, detail_level, enumeration, explicit_symptom, spelling_errors])
+
+        
+        df = pd.DataFrame(data, columns=["Dialogue_Generated", "symptom", "description", "meta", "language_style", "Tone", "Detail_level", "Enumeration", "Explicit_symptom", "Spelling_errors"])
+        
+        df.to_csv("dataset_generated_multi_symptoms_many_combinations.csv")
+
+        counter += 1
+        print(f"{counter}/{total} phrases generated", end='\r')  # Real-time update on same line
